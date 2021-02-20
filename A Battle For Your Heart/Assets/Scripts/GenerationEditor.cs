fe616fc559c315774dd773b624fc168d88a44024
EmersonSimmonds <Emerson.Simmonds@outlook.com>
2021-02-19 21:46:17 -0500

    Render some lines.
    Begun the psuedocode for rythm grid.

---
+++ A Battle For Your Heart/Assets/Scripts/GenerationEditor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(LevelGeneration))]
+public class GenerationEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        LevelGeneration lg = (LevelGeneration)target;
+
+        if (GUILayout.Button("Generate Lines"))
+        {
+            lg.GenerateLines();
+        }
+    }
+
+
+}
---
+++ A Battle For Your Heart/Assets/Scripts/LevelGeneration.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGeneration : MonoBehaviour
+{
+    public GameObject linePrefab;
+    public List<LineRenderer> lines;
+    
+    //How far appart you want?
+    public float lineSpacing;
+    //Local use variable
+    private float currentSpacing;
+    private GameObject currentLine;
+    public float bpm;
+
+    public Vector3 currentPosition;
+
+    public void GenerateLines()
+    {
+        //Clears the list of lines
+        lines.Clear();
+        
+        for (int i = 0; i < 4; i++)
+        {
+            lines.Add(Instantiate(linePrefab).GetComponent<LineRenderer>());
+            
+            //Spaces the lines
+            if(i == 0)
+            {
+                currentSpacing = 3;
+            }
+            else if (i == 1)
+            {
+                currentSpacing = 1;
+            }
+            else if (i == 2)
+            {
+                currentSpacing = -1;
+            }
+            else if (i == 3)
+            {
+                currentSpacing = -3;
+            }
+
+            //Sets the first position of the line
+            currentPosition = lines[i].GetPosition(0);
+            lines[i].SetPosition(0, new Vector3(currentSpacing * lineSpacing, currentPosition.y, currentPosition.z));
+            
+            //Sets the second position of the line
+            currentPosition = lines[i].GetPosition(1);
+            lines[i].SetPosition(1, new Vector3(currentSpacing * lineSpacing, currentPosition.y, currentPosition.z));
+
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if(lines.Count > 0 && lines[0] != null)
+        {
+            for (int i = 0; i < lines.Count; i++)
+            {
+                Gizmos.DrawRay(lines[i].GetPosition(0), Vector3.forward * 200);
+            }
+        }
+        
+    }
+
+
+}
