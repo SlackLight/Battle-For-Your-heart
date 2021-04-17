0b88588fadcfbfa1b1ebfeb0f85ec26819f0f291
ramirefr <ramirefr@sheridancollege.ca>
2021-04-16 22:27:43 -0400

    Code menu functions

--- A Battle For Your Heart/Assets/Scripts/MenuScene.cs
+++ A Battle For Your Heart/Assets/Scripts/MenuScene.cs
@@ -1,18 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuScene : MonoBehaviour
 {
+    public GameObject controlsPanel;
     // Start is called before the first frame update
     void Start()
     {
-        
+        controlsPanel.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    void PlayGame()
     {
-        
+        SceneManager.LoadScene(1);
+    }
+
+    void ControlsMenu()
+    {
+        controlsPanel.SetActive(true);
+    }
+
+    void CloseControls()
+    {
+        controlsPanel.SetActive(false);
     }
 }
