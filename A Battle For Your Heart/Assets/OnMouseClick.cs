b8ef1650b902c86259142b9401a61b959b721378
TristanPeskett <tristanpeskett@rogers.com>
2021-04-18 16:01:49 -0400

    started art minigame , added songs

---
+++ A Battle For Your Heart/Assets/OnMouseClick.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.Serialization;
+
+
+public class OnMouseClick : MonoBehaviour, IPointerDownHandler
+{
+
+    public UnityEvent OnMouse;
+
+
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log(this.gameObject.name + " Was Clicked.");
+    }
+
+
+    void OnMouseDown()
+    {
+        OnMouse.Invoke();
+    }
+    
+}
