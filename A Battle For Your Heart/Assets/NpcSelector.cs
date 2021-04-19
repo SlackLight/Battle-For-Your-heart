b5decfa35e01b043e6c3b505d5281089bf3cbe3d
Adrian Cogswell <cogs.adrian@gmail.com>
2021-04-19 15:35:59 -0400

    NPC selector completed, named characters will ignore settings for extras, mouth pose is set to public method in case characters need to change expressions during runtime

--- A Battle For Your Heart/Assets/NpcSelector.cs
+++ A Battle For Your Heart/Assets/NpcSelector.cs
@@ -12,7 +12,7 @@ public class NpcSelector : MonoBehaviour
     public enum handPoseSelect { relaxRelax, relaxFist,fistRelax,fistFist};
     public enum styleSelect { sockShort,sockLong,stockingShort,stockingLong};
     public enum clubSelect { noClub,fanClub};
-    public enum mouthSelect { negativeClose,negativeOpen,neutralClose,surprisedOpen,positiveClosed,positiveOpen};
+    public enum mouthSelect { neutralClose, surprisedOpen, negativeClose,negativeOpen,positiveClose,positiveOpen};
 
     public characterSelect characterSet;
     public mouthSelect mouthSet;
@@ -26,7 +26,6 @@ public class NpcSelector : MonoBehaviour
     private apControlParam mouth;
     private apControlParam head;
     private apControlParam clubBand;
-    [SerializeField]
     private apControlParam skinTone;
     private apControlParam frontHand;
     private apControlParam backHand;
@@ -35,7 +34,6 @@ public class NpcSelector : MonoBehaviour
     private apControlParam eyes;
     private apControlParam skirt;
 
-    //[SerializeField]
     apPortrait ap;
 
     void Awake()
@@ -56,19 +54,18 @@ public class NpcSelector : MonoBehaviour
             eyes = ap.GetControlParam("Extras EyeSet");
             skirt = ap.GetControlParam("SkirtStyle");
         }
-
-
-        
     }
+
     private void Start()
     {
         SetNPC();
+        SetMouth();
     }
 
     void SetNPC()
     {
         //set Ai
-        if(characterSet == characterSelect.Ai)
+        if (characterSet == characterSelect.Ai)
         {
             ap.SetControlParamInt(head, -4);
             ap.SetControlParamInt(clubBand, 0);
@@ -78,9 +75,8 @@ public class NpcSelector : MonoBehaviour
             ap.SetControlParamInt(legStyle, 2);
             ap.SetControlParamInt(skirt, 0);
         }
-
         //set Shou
-        else if(characterSet == characterSelect.Shou)
+        else if (characterSet == characterSelect.Shou)
         {
             ap.SetControlParamInt(head, -3);
             ap.SetControlParamInt(clubBand, 1);
@@ -102,23 +98,173 @@ public class NpcSelector : MonoBehaviour
             ap.SetControlParamInt(skirt, 1);
         }
         //set himeko
-        else if(characterSet == characterSelect.Himeko)
+        else if (characterSet == characterSelect.Himeko)
         {
             ap.SetControlParamInt(head, -1);
             ap.SetControlParamInt(clubBand, 1);
             ap.SetControlParamInt(skinTone, 1);
             ap.SetControlParamInt(frontHand, -2);
-            ap.SetControlParamInt(backHand, 2);
+            ap.SetControlParamInt(backHand, 4);
             ap.SetControlParamInt(legStyle, 1);
             ap.SetControlParamInt(skirt, 0);
         }
-    }
-
-    private void Update()
-    {
-        if (Input.GetKeyUp("q"))
+        //set extras
+        else
         {
-            SetNPC();
+            SetExtras();
         }
     }
+
+    void SetHands(int toneModifier)
+    {
+        /////////////
+        //set hands//
+        /////////////
+
+        if (extraHandPose == handPoseSelect.relaxRelax)
+        {
+            ap.SetControlParamInt(frontHand, -5 + toneModifier);
+            ap.SetControlParamInt(backHand, -6 + toneModifier);
+        }
+        else if (extraHandPose == handPoseSelect.relaxFist)
+        {
+            ap.SetControlParamInt(frontHand, -5 + toneModifier);
+            if (toneModifier>1)
+                ap.SetControlParamInt(backHand, -2 + toneModifier +1);
+            else
+                ap.SetControlParamInt(backHand, -2 + toneModifier);
+        }
+        else if (extraHandPose == handPoseSelect.fistRelax)
+        {
+            ap.SetControlParamInt(frontHand, 0 + toneModifier);
+            ap.SetControlParamInt(backHand, -6 + toneModifier);
+        }
+        else
+        {
+            ap.SetControlParamInt(frontHand, 0 + toneModifier);
+            if (toneModifier > 1)
+                ap.SetControlParamInt(backHand, -2 + toneModifier + 1);
+            else
+                ap.SetControlParamInt(backHand, -2 + toneModifier);
+        }
+    }
+
+    void SetExtras()
+    {
+        ////////////////////
+        //set skin & hands//
+        ////////////////////
+
+        if (extraSkinTone == skinToneSelect.T1)
+        {
+            ap.SetControlParamInt(head, 1);
+            ap.SetControlParamInt(skinTone, -3);
+            SetHands(0);
+        }
+        else if (extraSkinTone == skinToneSelect.T2)
+        {
+            ap.SetControlParamInt(head, 2);
+            ap.SetControlParamInt(skinTone, -2);
+            SetHands(1);
+        }
+        else if (extraSkinTone == skinToneSelect.T3)
+        {
+            ap.SetControlParamInt(head, 3);
+            ap.SetControlParamInt(skinTone, -1);
+            SetHands(2);
+        }
+        else if (extraSkinTone == skinToneSelect.T4)
+        {
+            ap.SetControlParamInt(head, 4);
+            ap.SetControlParamInt(skinTone, 1);
+            SetHands(3);
+        }
+
+        ////////////
+        //set eyes//
+        ////////////
+
+        if (extraEyes == eyeSetSelect.closed)
+            ap.SetControlParamInt(eyes, 3);
+        else if (extraEyes == eyeSetSelect.sleepy)
+            ap.SetControlParamInt(eyes, 2);
+        else
+            ap.SetControlParamInt(eyes, 1);
+        ////////////
+        //set hair//
+        ////////////
+
+        if (extraHair == hairSelect.twinTail)
+            ap.SetControlParamInt(hair, 1);
+        else if (extraHair == hairSelect.curlyTail)
+            ap.SetControlParamInt(hair, 2);
+        else if (extraHair == hairSelect.bob)
+            ap.SetControlParamInt(hair, 3);
+        else
+            ap.SetControlParamInt(hair, 4);
+
+        ////////////////////
+        //set skirt + legs//
+        ////////////////////
+
+        if (extraStyle == styleSelect.stockingLong || extraStyle == styleSelect.stockingShort)
+        {
+            ap.SetControlParamInt(legStyle, 3);
+            if (extraStyle == styleSelect.stockingLong)
+                ap.SetControlParamInt(skirt, 1);
+            else
+                ap.SetControlParamInt(skirt, 0);
+        }
+        else
+        {
+            //leg skin tones
+            if (extraSkinTone == skinToneSelect.T1)
+                ap.SetControlParamInt(legStyle, -3);
+            else if (extraSkinTone == skinToneSelect.T2)
+                ap.SetControlParamInt(legStyle, -2);
+            else if (extraSkinTone == skinToneSelect.T3)
+                ap.SetControlParamInt(legStyle, -1);
+            else if (extraSkinTone == skinToneSelect.T4)
+                ap.SetControlParamInt(legStyle, 1);
+
+            //skirt
+            if (extraStyle == styleSelect.sockLong)
+                ap.SetControlParamInt(skirt, 1);
+            else
+                ap.SetControlParamInt(skirt, 0);
+        }
+
+        ///////////////
+        //set armband//
+        ///////////////
+        if (clubSet == clubSelect.fanClub)
+            ap.SetControlParamInt(clubBand, 1);
+        else
+            ap.SetControlParamInt(clubBand, 0);
+    }
+
+    public void SetMouth()
+    {
+        if (mouthSet == mouthSelect.neutralClose)
+            ap.SetControlParamInt(mouth, 0);
+        else if (mouthSet == mouthSelect.surprisedOpen)
+            ap.SetControlParamInt(mouth, 1);
+        else if (mouthSet == mouthSelect.negativeClose)
+            ap.SetControlParamInt(mouth, -3);
+        else if (mouthSet == mouthSelect.negativeOpen)
+            ap.SetControlParamInt(mouth, -2);
+        else if (mouthSet == mouthSelect.positiveClose)
+            ap.SetControlParamInt(mouth, 2);
+        else if (mouthSet == mouthSelect.positiveOpen)
+            ap.SetControlParamInt(mouth, 3);
+    }
+
+    //private void Update()
+    //{
+    //    if (Input.GetKeyUp("q"))
+    //    {
+    //        SetNPC();
+    //        SetMouth();
+    //    }
+    //}
 }
