5f853b7d58f8eca3249cfef152c2a91b97a597ff
ramirefr <ramirefr@sheridancollege.ca>
2021-04-20 19:45:15 -0400

    Connecting scenes

---
+++ A Battle For Your Heart/Assets/Scripts/LoadScenes.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScenes : MonoBehaviour
+{
+    [SerializeField] string sceneToLoad;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Invoke("LoadScene", 3);
+    }
+
+    void LoadScene()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
